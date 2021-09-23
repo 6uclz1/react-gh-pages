81c9d75b243c67ba41ecf4e56224a005462d66cc
6uclz1 <6uclz1@gmail.com>
2021-09-24 02:44:00 +0900

    style applied :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import './scss/global.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='header'>
+        </div>
+        <div className='container'>
+          <div className='box'>
+            <div className='text'>
+              <h1>test</h1>
+              <p>testtest testtesttestt esttesttesttest.</p>
+              <div className='btn'>
+                <div className='tooltip'>
+                  <a className='cta'>
+                    read more
+                  </a>
+                  <span className='tooltiptext'>open new page</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='box'>
+            <div className='text'>
+              <h1>test</h1>
+              <p>testtestte sttesttesttest testtesttest. testtesttes ttesttest.</p>
+              <div className='btn'>
+                <div className='tooltip'>
+                  <a className='cta'>
+                    read more
+                  </a>
+                  <span className='tooltiptext'>open new page</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='box'>
+          </div>
+          <div className='box'>
+          </div>
+          <div className='box'>
+          </div>
+          <div className='box'>
+          </div>
+        </div>
+        <div className='header'>
+        </div>
       </header>
     </div>
   );
